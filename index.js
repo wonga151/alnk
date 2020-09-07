1e82d78c57237f1a4a15ab6b878deb7e53fce6d3
A W <caramelvanillacrunch@gmail.com>
2020-09-06 20:56:18 -0700

    Add get and post requests with db validation on unique urls

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const yup = require('yup');
+const monk = require('monk');
+const { nanoid } = require('nanoid');
+
+require('dotenv').config();
+
+const db = monk(process.env.MONGO_URI);
+const urls = db.get('urls');
+
+urls.createIndex({ slug: 1 }, { unique: true });
 
 const app = express();
 
@@ -21,13 +31,67 @@ app.use(express.static('./public'));
 //   // TODO: get a short url by id
 // })
 
-// app.get('/:id', (req, res) => {
-//   // TODO: redirect to url
-// })
+app.get('/:id', async (req, res) => {
+  const { id: slug } = req.params;
 
-// app.get('/url', (req, res) => {
-//   // TODO: create a short url
-// })
+  try {
+    const url = await urls.findOne({ slug });
+    if (url) {
+      res.redirect(url.url);
+    }
+    res.redirect(`/?error=${slug} not found`);
+  } catch (error) {
+    res.redirect(`/?error=Link not found`);
+  }
+})
+
+
+const schema = yup.object().shape({
+  slug: yup.string().trim().matches(/[\w\-]/i),
+  url: yup.string().trim().url().required(),
+})
+
+app.post('/url', async (req, res, next) => {
+  let { slug, url } = req.body;
+
+  try {
+    await schema.validate({
+      slug,
+      url
+    })
+
+    if (!slug) {
+      slug = nanoid(5);
+    }
+
+    slug = slug.toLowerCase();
+    const newUrl = {
+      slug,
+      url
+    }
+
+    const created = await urls.insert(newUrl);
+    res.json(created)
+
+  } catch (error) {
+    if (error.message.startsWith("E11000")) {
+      error.message = "Slug in use."
+    }
+    next(error)
+  }
+})
+
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status)
+  } else {
+    res.status(500);
+  }
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? 'error' : error.stack
+  })
+})
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
