7bceaae0f2e0b3fc026be8fa7a0754e5868278c7
A W <caramelvanillacrunch@gmail.com>
2020-11-12 22:26:53 -0800

    Add back redis connection

---
+++ initRedis.js
@@ -0,0 +1,26 @@
+const redis = require('redis')
+const redisClient = redis.createClient(process.env.REDIS_URL);
+// const redisClient = redis.createClient()
+
+// redisClient.flushall(function (err, succeeded) {
+//   console.log(succeeded); // will be true if successfull
+// });
+
+redisClient.on('connect', () => {
+  console.log("Client connected to Redis...")
+})
+
+redisClient.on('ready', () => {
+  console.log("Client connected to Redis and ready to use...")
+})
+
+redisClient.on('error', (error) => {
+  console.log(error.message)
+})
+
+redisClient.on('end', () => {
+  console.log("Client has been disconnected from Redis...")
+})
+
+module.exports = redisClient
+
